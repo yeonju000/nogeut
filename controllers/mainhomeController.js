f4451e8522c5937fd2e229139fc8618b3f486fb7
yeonju000 <20221146@sungshin.ac.kr>
2024-06-03 10:51:06 +0000

    mainhome

--- controllers/mainhomeController.js
+++ controllers/mainhomeController.js
@@ -9,10 +9,7 @@ module.exports = {
   
     handleLogin: (req, res) => {
       const { userID, password } = req.body;
-      // 로그인 인증 로직 추가 (예: DB 조회 및 비밀번호 검증)
-      // 여기에 로그인 검증 로직을 추가하십시오.
-      // 예를 들어, DB에서 사용자 정보 조회 및 비밀번호 검증 등.
-      // 여기서는 간단한 예시로 하드코딩된 값을 사용합니다.
+      //로그인 인증 로직 추가해야함!!!!!!!!!!
       if (userID === "user" && password === "pass") {
         res.redirect("/mainhome");
       } else {
